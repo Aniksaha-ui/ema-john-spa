7a384ec75fd3b783ec166f98caf002176df2b0e4
Aniksaha-ui <sahaanik1045@gmail.com>
2022-03-25 11:56:22 +0600

    save to local storage and retrive data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
 import Product from "../Products/Product";
 import "./Shop.css";
 const Shop = () => {
@@ -7,13 +9,33 @@ const Shop = () => {
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   useEffect(() => {
+    // console.log("product loading.......");
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+
+  useEffect(() => {
+    const storedCart = getStoredCart();
+    const savedCart = [];
+    // console.log(storedCart);
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      // console.log(addedProduct);
+      if (addedProduct) {
+        console.log(storedCart[id]);
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        savedCart.push(addedProduct);
+      }
+    }
+
+    setCart(savedCart);
+  }, [products]);
 
   return (
     <div className="shop-container">
@@ -27,8 +49,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <h4>Order Summery</h4>
-        <p>Selected Item : {cart.length}</p>
+        <Cart cart={cart} />
       </div>
     </div>
   );
